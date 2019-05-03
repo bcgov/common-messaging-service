b416d4cedcbe769713c7aed6bc334c38bd0d9692
Jason Sherman <jsherman@parcsystems.ca>
2019-05-03 11:24:45 -0700

    formatting fix. atom - for shame... not quite formatting correctly.

--- app/app.js
+++ app/app.js
@@ -9,27 +9,27 @@ app.use(express.static('static'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/index.html', function(req, res) {
+app.get('/index.html', function (req, res) {
   res.sendFile(__dirname + "/static/index.html");
-  });
+});
 
 // Handle 500
-app.use(function(err, _, res, _) {
-    log.error(err.stack)
-    res.status(500);
-    res.json({
-        status: 500,
-        message: 'Internal Server Error: ' + err.stack.split('\n', 1)[0]
-    });
+app.use(function (err, _, res, _) {
+  log.error(err.stack)
+  res.status(500);
+  res.json({
+    status: 500,
+    message: 'Internal Server Error: ' + err.stack.split('\n', 1)[0]
+  });
 });
 
 // Handle 404
-app.use(function(_, res) {
-    res.status(404);
-    res.json({
-        status: 404,
-        message: 'Page Not Found'
-    });
+app.use(function (_, res) {
+  res.status(404);
+  res.json({
+    status: 404,
+    message: 'Page Not Found'
+  });
 });
 
 module.exports = app;
