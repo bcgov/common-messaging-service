5fbfba702cf72ad2a0909f16cd7d55fd830e5c7e
Jason Sherman <jsherman@parcsystems.ca>
2019-05-10 10:40:47 -0700

    update eslint settings, run and fix across files.  still using a more modern version of ecmaVersion until I figure out React and async issues.

--- frontend/src/EmailForm.jsx
+++ frontend/src/EmailForm.jsx
@@ -15,12 +15,12 @@ class EmailForm extends Component {
       hasCreateMessage: false,
       cmsgApiHealthy: false,
       wasValidated: false,
-      sender: "NR.CommonServiceShowcase@gov.bc.ca",
-      recipients: "",
-      subject: "",
-      body: "",
-      info: "",
-      error: ""
+      sender: 'NR.CommonServiceShowcase@gov.bc.ca',
+      recipients: '',
+      subject: '',
+      body: '',
+      info: '',
+      error: ''
     };
 
     this.formSubmit = this.formSubmit.bind(this);
@@ -30,15 +30,15 @@ class EmailForm extends Component {
   }
 
   onChangeSubject(event) {
-    this.setState({subject: event.target.value, info: ""});
+    this.setState({subject: event.target.value, info: ''});
   }
 
   onChangeRecipients(event) {
-    this.setState({recipients: event.target.value, info: ""});
+    this.setState({recipients: event.target.value, info: ''});
   }
 
   onChangeBody(event) {
-    this.setState({body: event.target.value, info: ""});
+    this.setState({body: event.target.value, info: ''});
   }
 
   async componentDidMount() {
@@ -57,7 +57,6 @@ class EmailForm extends Component {
       if (hasTopLevel) {
         let json = await this.healthCheck(token, defaultConfiguration.urls.root);
         cmsgApiHealthy = json['@type'] === 'http://nrscmsg.nrs.gov.bc.ca/v1/endpoints';
-        ;
       }
 
       this.setState({
@@ -80,7 +79,7 @@ class EmailForm extends Component {
         error: e.message
       });
     }
-  };
+  }
 
   async pingMessageService() {
     const response = await fetch(`${MSG_SERVICE_PATH}/config`);
@@ -123,9 +122,9 @@ class EmailForm extends Component {
       url = this.state.config.urls.root;
     }
     let headers = new Headers();
-    headers.set("Authorization", `Bearer ${token}`);
-    headers.set("Content-Type", "application/json");
-    const response = await fetch(url, {method: "get", headers: headers});
+    headers.set('Authorization', `Bearer ${token}`);
+    headers.set('Content-Type', 'application/json');
+    const response = await fetch(url, {method: 'get', headers: headers});
     if (!response.ok) {
       throw Error('Could not connect to Common Messaging Service: ' + response.statusText);
     }
@@ -139,7 +138,7 @@ class EmailForm extends Component {
     event.stopPropagation();
 
     if (!event.target.checkValidity()) {
-      this.setState({wasValidated: true, info: ""});
+      this.setState({wasValidated: true, info: ''});
       return;
     }
 
@@ -152,22 +151,22 @@ class EmailForm extends Component {
       }
       this.setState({
         wasValidated: false,
-        recipients: "",
-        subject: "",
-        body: "",
-        info: "Message submitted to Common Messaging Service"
+        recipients: '',
+        subject: '',
+        body: '',
+        info: 'Message submitted to Common Messaging Service'
       });
     } catch (e) {
       this.setState({
         wasValidated: false,
-        info: "",
+        info: '',
         error: e.message
       });
     }
 
     window.scrollTo(0, 0);
 
-  };
+  }
 
   async postEmail(token, url) {
     if (!url) {
@@ -184,20 +183,20 @@ class EmailForm extends Component {
         "maxResend": 0,
         "mediaType": "text/plain"
     }
-    `
+    `;
     // Add the user entered fields
     const requestBody = JSON.parse(defaults);
     requestBody.sender = this.state.sender;
     requestBody.subject = this.state.subject;
     requestBody.message = this.state.body;
-    requestBody.recipients = this.state.recipients.replace(/\s/g, '').split(",");
+    requestBody.recipients = this.state.recipients.replace(/\s/g, '').split(',');
 
     const headers = new Headers();
-    headers.set("Authorization", `Bearer ${token}`);
-    headers.set("Content-Type", "application/json");
+    headers.set('Authorization', `Bearer ${token}`);
+    headers.set('Content-Type', 'application/json');
 
     let response = await fetch(url, {
-      method: "POST",
+      method: 'POST',
       body: JSON.stringify(requestBody),
       headers: headers
     });
@@ -230,25 +229,24 @@ class EmailForm extends Component {
 
   async fetchStatus(token, statusHref) {
     const headers = new Headers();
-    headers.set("Authorization", `Bearer ${token}`);
-    headers.set("Content-Type", "application/json");
+    headers.set('Authorization', `Bearer ${token}`);
+    headers.set('Content-Type', 'application/json');
 
     await this.sleep(100);
     let result = await fetch(statusHref, {
-      method: "GET",
+      method: 'GET',
       headers: headers
     }).then(res => res.json());
     return result;
 
   }
 
-
   render() {
     // set styles and classes here...
-    const credentialsIndClass = this.state.credentialsGood ? "icon good" : "icon bad";
-    const apiAccessIndClass = this.state.hasTopLevel ? "icon good" : "icon bad";
-    const createMsgIndClass = this.state.hasCreateMessage ? "icon good" : "icon bad";
-    const healthCheckIndClass = this.state.cmsgApiHealthy ? "icon good" : "icon bad";
+    const credentialsIndClass = this.state.credentialsGood ? 'icon good' : 'icon bad';
+    const apiAccessIndClass = this.state.hasTopLevel ? 'icon good' : 'icon bad';
+    const createMsgIndClass = this.state.hasCreateMessage ? 'icon good' : 'icon bad';
+    const healthCheckIndClass = this.state.cmsgApiHealthy ? 'icon good' : 'icon bad';
     const emailFormDisplay = this.state.hasCreateMessage ? {} : {display: 'none'};
     const successDisplay = (this.state.info && this.state.info.length > 0) ? {} : {display: 'none'};
     const errorDisplay = (this.state.error && this.state.error.length > 0) ? {} : {display: 'none'};
@@ -268,33 +266,33 @@ class EmailForm extends Component {
           <hr className="mb-4"/>
           <div className="row">
             <span className="col-md-6 hc-text">Service Client credentials</span><span
-            className="col-md-6 hc-ind"><span id="credentialsInd"
-                                              className={credentialsIndClass}></span></span>
+              className="col-md-6 hc-ind"><span id="credentialsInd"
+                className={credentialsIndClass}></span></span>
           </div>
           <div className="row">
-                            <span
-                              className="col-md-6 hc-text">Service Client has access to Common Messaging API</span><span
-            className="col-md-6 hc-ind"><span id="apiAccessInd" className={apiAccessIndClass}></span></span>
+            <span
+              className="col-md-6 hc-text">Service Client has access to Common Messaging API</span><span
+              className="col-md-6 hc-ind"><span id="apiAccessInd" className={apiAccessIndClass}></span></span>
           </div>
           <div className="row">
             <span className="col-md-6 hc-text">Service Client can send message</span><span
-            className="col-md-6 hc-ind"><span id="createMsgInd" className={createMsgIndClass}></span></span>
+              className="col-md-6 hc-ind"><span id="createMsgInd" className={createMsgIndClass}></span></span>
           </div>
           <div className="row">
             <span className="col-md-6 hc-text">Common Messaging API available</span><span
-            className="col-md-6 hc-ind"><span id="healthCheckInd"
-                                              className={healthCheckIndClass}></span></span>
+              className="col-md-6 hc-ind"><span id="healthCheckInd"
+                className={healthCheckIndClass}></span></span>
           </div>
         </div>
 
         <hr className="mb-4"/>
         <form id="emailForm" noValidate style={emailFormDisplay} onSubmit={this.formSubmit}
-              className={wasValidated ? 'was-validated' : ''}>
+          className={wasValidated ? 'was-validated' : ''}>
           <h4 className="mb-3">Email</h4>
           <div className="mb-3">
             <label htmlFor="sender">Sender</label>
             <input type="text" className="form-control" name="sender"
-                   readOnly required value={this.state.sender}/>
+              readOnly required value={this.state.sender}/>
             <div className="invalid-feedback">
               Email sender is required.
             </div>
@@ -303,8 +301,8 @@ class EmailForm extends Component {
           <div className="mb-3">
             <label htmlFor="recipients">Recipients</label>
             <input type="text" className="form-control" name="recipients"
-                   placeholder="you@example.com (separate multiple by comma)" required
-                   value={this.state.recipients} onChange={this.onChangeRecipients}/>
+              placeholder="you@example.com (separate multiple by comma)" required
+              value={this.state.recipients} onChange={this.onChangeRecipients}/>
             <div className="invalid-feedback">
               One or more email recipients required.
             </div>
@@ -313,7 +311,7 @@ class EmailForm extends Component {
           <div className="mb-3">
             <label htmlFor="subject">Subject</label>
             <input type="text" className="form-control" name="subject" required value={this.state.subject}
-                   onChange={this.onChangeSubject}/>
+              onChange={this.onChangeSubject}/>
             <div className="invalid-feedback">
               Subject is required.
             </div>
@@ -322,7 +320,7 @@ class EmailForm extends Component {
           <div className="mb-3">
             <label htmlFor="body">Body</label>
             <textarea name="body" className="form-control" cols="30" rows="7" required
-                      value={this.state.body} onChange={this.onChangeBody}></textarea>
+              value={this.state.body} onChange={this.onChangeBody}></textarea>
             <div className="invalid-feedback">
               Email body is required.
             </div>
--- frontend/src/Footer.jsx
+++ frontend/src/Footer.jsx
@@ -8,17 +8,17 @@ class Footer extends Component {
         <ul className="list-inline">
           <li className="list-inline-item"><a id='footer-home' href="https://www.gov.bc.ca/">Home</a></li>
           <li className="list-inline-item"><a id="footer-about"
-                                              href="https://www2.gov.bc.ca/gov/content/about-gov-bc-ca">About
+            href="https://www2.gov.bc.ca/gov/content/about-gov-bc-ca">About
             gov.bc.ca</a></li>
           <li className="list-inline-item"><a id="footer-disclaimer"
-                                              href="http://gov.bc.ca/disclaimer">Disclaimer</a>
+            href="http://gov.bc.ca/disclaimer">Disclaimer</a>
           </li>
           <li className="list-inline-item"><a id="footer-privacy" href="http://gov.bc.ca/privacy">Privacy</a>
           </li>
           <li className="list-inline-item"><a id="footer-accessibility"
-                                              href="http://gov.bc.ca/webaccessibility">Accessibility</a></li>
+            href="http://gov.bc.ca/webaccessibility">Accessibility</a></li>
           <li className="list-inline-item"><a id="footer-copyright"
-                                              href="http://gov.bc.ca/copyright">Copyright</a>
+            href="http://gov.bc.ca/copyright">Copyright</a>
           </li>
           <li className="list-inline-item"><a href="https://www2.gov.bc.ca/gov/content/home/contact-us">Contact
             Us</a>
