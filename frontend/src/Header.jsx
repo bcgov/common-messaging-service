3d9a3907e6f81a2f21053c9fcc6a375a8efe8ad2
Jason Sherman <jsherman@parcsystems.ca>
2019-06-10 20:47:12 -0700

    no message

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -6,7 +6,7 @@ class Header extends Component {
       <header className="py-5 text-center">
         <img className="d-block mx-auto" src="img/logo.png" alt="BC Gov" height="100px"/>
         <h2>Common Messaging Service Showcase App</h2>
-        <p className="lead">This is a simple application, showcasing the Email-sending facility provided by Common Messaging Service. It requires a Service Client that has
+        <p className="lead">This is a simple application, showcasing the Email-sending facility provided by Common Messaging Service.<br/>It requires a Service Client that has
           previously been created in the environment with appropriate CMSG scopes.</p>
       </header>
     );
