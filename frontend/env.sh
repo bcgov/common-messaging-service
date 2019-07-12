5c84de483fd3916605fd62e625755ff3dba566b5
Jason Sherman <jsherman@parcsystems.ca>
2019-07-11 18:08:44 -0700

    no message

--- frontend/env.sh
+++ frontend/env.sh
@@ -11,19 +11,7 @@ echo "window._env_ = {" >> ./env-config.js
 # Each line represents key=value pairs
 while IFS="" read -r line
 do
-  # Split env variables by character `=`
-  if printf '%s\n' "$line" | grep -q -e '='; then
-    varname=$(printf '%s\n' "$line" | sed -e 's/=.*//')
-    varvalue=$(printf '%s\n' "$line" | sed -e 's/^[^=]*=//')
-  fi
-
-  # Read value of current variable if exists as Environment variable
-  value=$(printf '%s\n' "${!varname}")
-  # Otherwise use value from .env file
-  [[ -z $value ]] && value=${varvalue}
-
-  # Append configuration property to JS file
-  echo "  $varname: '$value'," >> ./env-config.js
+  echo "read from file: $line"
 done < .env
 
 echo "};" >> ./env-config.js
