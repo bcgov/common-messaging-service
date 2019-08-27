10bbba0fd74758f24209b6d233b1017cb8f0e20d
Jason Sherman <jsherman@parcsystems.ca>
2019-08-27 12:03:17 -0700

    add a liveness probe endpoint

--- ches-backend/chesService/routes.js
+++ ches-backend/chesService/routes.js
@@ -3,6 +3,10 @@ const wrap = require('../middleware/wrap');
 
 const {sendEmail} = require('./controller');
 
+routes.get('/', wrap(function (req, res, next) {
+  res.status(200).json({alive: true});
+}));
+
 routes.post('/email', wrap(async function (req, res, next) {
   await sendEmail(req, res, next);
 }));
