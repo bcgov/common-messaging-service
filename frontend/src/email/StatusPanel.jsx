058f443f9b6b57aa5f3e6018aae8ab2486e9d253
Jason Sherman <jsherman@parcsystems.ca>
2019-10-29 09:52:15 -0700

    limit callback params to ones used.

--- frontend/src/email/StatusPanel.jsx
+++ frontend/src/email/StatusPanel.jsx
@@ -22,7 +22,7 @@ class StatusPanel extends Component {
 
   async asyncForEach(array, callback) {
     for (let index = 0; index < array.length; index++) {
-      await callback(array[index], index, array);
+      await callback(array[index]);
     }
   }
 
