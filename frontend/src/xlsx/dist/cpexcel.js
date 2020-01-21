5903f71be3cc4941edcfb3739f9797822ec3d3cc
Jason Sherman <jsherman@parcsystems.ca>
2020-01-20 18:28:52 -0800

    turn off lint errors for react build

--- frontend/src/xlsx/dist/cpexcel.js
+++ frontend/src/xlsx/dist/cpexcel.js
@@ -1,3 +1,4 @@
+/*eslint-disable */
 /* cpexcel.js (C) 2013-present SheetJS -- http://sheetjs.com */
 /*jshint -W100 */
 var cptable = {version:"1.14.0"};
--- frontend/src/xlsx/xlsx.js
+++ frontend/src/xlsx/xlsx.js
@@ -1,3 +1,4 @@
+/*eslint-disable */
 /*! xlsx.js (C) 2013-present SheetJS -- http://sheetjs.com */
 /* vim: set ts=2: */
 /*exported XLSX */
