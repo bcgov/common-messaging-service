23a892421fb29ed402ff75b4777d87f0e68cda9a
Jason Sherman <jsherman@parcsystems.ca>
2020-01-21 08:26:22 -0800

    add comments to merge about related to dates and parsing. update example to include bad dates, move to end of file.

--- frontend/src/ches/MergeAbout.jsx
+++ frontend/src/ches/MergeAbout.jsx
@@ -26,7 +26,13 @@ class MergeAbout extends Component {
         <p>To showcase CHES, we have added some nice features to turn an Excel spreadsheet or CVS file into a
           CHES Mail Merge request. See below for some sample data and a template you can use as a guide to
           creating your own data and templates. It is advised that you fix the data in your spreadsheet and not
-          in the JSON editor.</p>
+          in the JSON editor.
+        </p>
+        <p>
+        The Excel spreadsheet/CVS file parser does best guesses on dates and times, it cannot parse and understand all
+          formats.  If you encounter difficulties with dates and times from Excel, convert your sheet to CSV and format
+          your dates to &apos;YYYY-MM-DD&apos; and time stamps to &apos;YYYY-MM-DD HH:mm&apos;.
+        </p>
         <p>
           <a href={process.env.PUBLIC_URL + '/docs/mssc-ches-merge-example.csv'} download>example csv</a><br/>
           <a href={process.env.PUBLIC_URL + '/docs/mssc-ches-merge-example.txt'} download>example html
@@ -62,7 +68,9 @@ class MergeAbout extends Component {
           <li>Delay TS is a timestamp of when to deliver the message. Leave empty if you wish to deliver now.
           </li>
           <li>Any other field that contains a date, the Excel parser will translate to YYYY-MM-DD. Look in the
-            csv file to see various formats it can translate.
+            csv file to see various formats it can translate.  <strong>Note:</strong> in the example that line 5 has date
+            formats that cannot be parsed; the Start Date and End Date TS show the unparsed data values, and the Delay TS
+            is ignored.
           </li>
           <li>If a field contains a date and time, and ends with ts (see endDateTs), MSSC will translate to
             YYYY-MM-DD hh:mm. This is local time.
@@ -70,14 +78,14 @@ class MergeAbout extends Component {
           <li>Note that delayTs appears in the JSON as a number. This is the date and time as milliseconds in
             UTC. This is what CHES expects.
           </li>
-          <li>Also, note down the lefthand side of the body is a listing of the variables you can use in the
+          <li>Also, note down the left hand side of the body is a listing of the variables you can use in the
             template. These are the CSV headings (altered if needed by MSSC)
           </li>
           <li>In Preview, you can navigate through all the messages that will be delivered.</li>
           <li><strong>Important:</strong> If you alter the JSON, the Excel data table will go away. This is a
             one way operation (excel to JSON).
           </li>
-        </ul>      
+        </ul>
       </div>
     );
   }
